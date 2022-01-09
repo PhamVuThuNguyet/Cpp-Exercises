0c871146a5cc814e90dc1436c735de2181650cd3
PhamVuThuNguyet <pvtnguyet.19it1vku@gmail.com>
2022-01-09 17:50:59 +0700

    Update ex7 chapter 7

---
+++ Code/Chapter7/Ex7.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    map<int, int> arr;
+    for (int i = 0; i < n; i++)
+    {
+        int temp;
+        cin >> temp;
+        arr[temp]++;
+    }
+
+    for (auto &x : arr)
+    {
+        if (x.second >= 2)
+        {
+            cout << x.first << " ";
+        }
+    }
+}
