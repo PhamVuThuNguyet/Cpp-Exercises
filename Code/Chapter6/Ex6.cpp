8b238ff0af3c80bda298bea1b322d9d7a56fe556
PhamVuThuNguyet <pvtnguyet.19it1vku@gmail.com>
2022-01-13 22:40:01 +0700

    update ex6 chapter 6

--- Code/Chapter6/Ex6.cpp
+++ Code/Chapter6/Ex6.cpp
@@ -10,21 +10,30 @@ void sieveSundaram()
     bool marked[MAX / 2 + 1] = {0};
 
     for (int i = 1; i <= (sqrt(MAX) - 1) / 2; i++)
-        for (int j = (i * (i + 1)) << 1;
-             j <= MAX / 2; j = j + 2 * i + 1)
+    {
+        for (int j = (i * (i + 1)) << 1; j <= MAX / 2; j = j + 2 * i + 1)
+        {
             marked[j] = true;
+        }
+    }
 
     primes.push_back(2);
 
     for (int i = 1; i <= MAX / 2; i++)
+    {
         if (marked[i] == false)
+        {
             primes.push_back(2 * i + 1);
+        }
+    }
 }
 
 int isEconomical(int n)
 {
     if (n == 1)
+    {
         return -1;
+    }
 
     int original_no = n;
     int sumDigits = 0;
