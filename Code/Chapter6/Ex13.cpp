8ac1fb81cb0ccd8ea531db8b06d7699f0415b455
PhamVuThuNguyet <pvtnguyet.19it1vku@gmail.com>
2022-01-13 22:14:58 +0700

    update ex13 chapter 6

---
+++ Code/Chapter6/Ex13.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void swapNumber(int *a, int *b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+    swapNumber(&a, &b);
+    cout << a << " " << b << endl;
+}
